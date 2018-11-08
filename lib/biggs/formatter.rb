f4ed00f0c9a6dde05b44c3a7d1e2485f9c3a44f8
Eric-Guo <eric@managebac.com>
2018-11-08 14:06:15 +0800

    Further code style improvement.

--- lib/biggs/formatter.rb
+++ lib/biggs/formatter.rb
@@ -1,21 +1,21 @@
 module Biggs
   class Formatter
-    FIELDS = [:recipient, :street, :city, :state, :zip, :country]
+    FIELDS = [:recipient, :street, :city, :state, :zip, :country].freeze
 
-    def initialize(options={})
+    def initialize(options = {})
       @blank_country_on = [options[:blank_country_on]].compact.flatten.map { |s| s.to_s.downcase }
       @school_country_id = options[:school_country_id]
     end
 
-    def format(iso_code, values={})
+    def format(iso_code, values = {})
       values.symbolize_keys! if values.respond_to?(:symbolize_keys!)
 
       format = Biggs::Format.find(@school_country_id, iso_code)
       format_string = (format.format_string || default_format_string(values[:state])).dup.to_s
-      country_name = blank_country_on.include?(format.iso_code) ? "" : format.country_name || format.iso_code
+      country_name = blank_country_on.include?(format.iso_code) ? '' : format.country_name || format.iso_code
 
       (FIELDS - [:country]).each do |key|
-        format_string.gsub!(/\{\{#{key}\}\}/, (values[key] || "").to_s)
+        format_string.gsub!(/\{\{#{key}\}\}/, (values[key] || '').to_s)
       end
       format_string.gsub!(/\{\{country\}\}/, country_name) unless country.nil?
       format_string.gsub(/\n$/, '')
@@ -26,9 +26,9 @@ module Biggs
     private
 
     def default_format_string(state)
-      state && state != "" ?
-        Biggs.formats[default_country_with_state || "us"] :
-        Biggs.formats[default_country_without_state || "fr"]
+      state && state != '' ?
+        Biggs.formats[default_country_with_state || 'us'] :
+        Biggs.formats[default_country_without_state || 'fr']
     end
   end
 end
